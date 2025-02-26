ef834a5f6eece35e4830213b653d3f4f291f1b3e
nex2link <nex2link@gmail.com>
2025-02-26 12:51:04 +0530

    Update AboutUsPage.jsx

--- src/pages/AboutUsPage.jsx
+++ src/pages/AboutUsPage.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
-import { useSmootherScroll } from '../hooks/useSmootherScroll';
-import { applyScrollOptimizations } from '../utils/scrollOptimizer';
-import { useEffect } from 'react';
 import OurMission from '../components/OurMission';
 import OurExperts from '../components/OurExperts';
 
 const AboutUsPage = () => {
-   useSmootherScroll();
-    
-    useEffect(() => {
-      applyScrollOptimizations();
-    }, []);
+
 
   return (
     <div className="container mx-auto px-4 py-8 mt-14">
-      <div className="scroll-content">
+      <div>
       <OurMission/>
       <OurExperts/> 
       </div>
